b6d3fe271f859ffec817d479d2279acd511065fa
Heejung Chung <16hchung@gmail.com>
2017-11-02 16:45:34 -0700

    Initial commit

---
+++ Munch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Munch
+//
+//  Created by Heejung Chung on 11/2/17.
+//  Copyright © 2017 hjchung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
